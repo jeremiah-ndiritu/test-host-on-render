f7250ef4446a875b1a39173e3cbab8a61739206a
jeremiah-ndiritu <ndiritujeremiah24@gmail.com>
2025-09-05 10:22:18 +0300

    serve homepage from a public folder

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-let PORT = process.env.PORT;
+let PORT = process.env.PORT || 8082;
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "homePage.html"));
+  res.sendFile(path.join(__dirname, "public", "homePage.html"));
+});
+
+app.get("/api/names", (req, res) => {
+  let names = ["Jeremiah", "Ndiritu", "Kiricu"];
+  res.json({ names });
 });
 
 app.listen(PORT, () => {
