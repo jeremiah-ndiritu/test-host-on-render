8e6cc0233707cf8384b36ff4d6e3f5dbf2a1eb0c
jeremiah-ndiritu <ndiritujeremiah24@gmail.com>
2025-09-05 11:53:57 +0300

    fix variable app bug

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-app.use(cors());
 
 const app = express();
+app.use(cors());
 let PORT = process.env.PORT || 8082;
 
 app.use(express.static(path.join(__dirname, "public")));
